a81a5ba07f78941f423bfd9a1e3126e8569ded5d
StevenKatabalwa <StevenKatabalwa2019@gmail.com>
2019-07-23 23:27:42 -0500

    created shopping cart and item object

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const { ShoppingCart } = require('./domain/ShoppingCart')
+const { Item } = require('./domain/Item')
+
+let app = express();
+
+let item1 = new Item.ItemBuilder()
+    .setName("Sugar")
+    .setUnitPrice(3.5)
+    .setDiscount(0.1)
+    .build();
+
+let item2 = new Item.ItemBuilder()
+    .setName("salt")
+    .setUnitPrice(1.5)
+    .setDiscount(0.25)
+    .build();
+
+let item3 = new Item.ItemBuilder()
+    .setName("shoes")
+    .setDiscount(1)
+    .setUnitPrice(35.9)
+    .build();
+
+let cart = new ShoppingCart.CartBuilder()
+    .addItem(item1, 4)
+    .addItem(item2, 11)
+    .addItem(item3, 5)
+    .build();
+
+console.log(cart.calculateTotalPrice())
+
+app.listen(8081, "localhost").on('listening', () => {
+
+    console.log("Server is listening")
+
+});
